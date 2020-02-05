d5ae01f387c30a944a768eddd9b0a73a6695f956
ijuhom <2014brand@naver.com>
2020-02-05 10:43:31 +0900

    async await 으로 변경

--- server/api/auth/auth.ctrl.js
+++ server/api/auth/auth.ctrl.js
@@ -1,56 +1,95 @@
 const User = require("../../models/user");
 
-const login = (req, res) => {
+const login = async (req, res) => {
   const { name, password } = req.body;
 
-  if (!name || !password) return res.status(401).json({ error: "아이디 혹은 비밀번호를 입력해주세요" });
-  User.findByName(name)
-    .then(exists => {
-      if (!exists) return res.status(401).json({ error: "존재하지않는 아이디입니다." });
-      else {
-        exists.comparePassword(password).then(isMatch => {
-          if (!isMatch)
-            return res.status(401).json({ error: "비밀번호가 일치하지 않습니다." });
-          else {
-            const token = exists.generateToKen();
-            res.cookie("access_token", token, {
-              httpOnly: true,
-              maxAge: 1000 * 60 * 60 * 24 * 7
-            });
-            return res.status(200).json(exists);
-          }
-        });
+  if (!name || !password)
+    return res
+      .status(401)
+      .json({ error: "아이디 혹은 비밀번호를 입력해주세요" });
 
-        // console.log("passwor", exists);
-      }
-    })
-    .catch(e => console.log(e));
+  try {
+    const user = await User.findByName(name);
+    if (!user) {
+      return res.status(401).json({ error: "존재하지않는 아이디입니다." });
+    }
+    const compare = await user.comparePassword(password);
+    if (!compare) {
+      return res.status(401).json({ error: "비밀번호가 일치하지 않습니다." });
+    }
+    const token = await user.generateToKen();
+    res.cookie("access_token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    });
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+  }
+
+  // User.findByName(name)
+  //   .then(user => {
+  //     if (!user)
+  //       return res.status(401).json({ error: "존재하지않는 아이디입니다." });
+  //     return user.comparePassword(password);
+  //   })
+  //   .then(isMatch => {
+  //     if (!isMatch) {
+  //       return res.status(401).json({ error: "비밀번호가 일치하지 않습니다." });
+  //     }
+  //     return User.findByName(name);
+  //   })
+  //   .then(user => {
+  //     const token = user.generateToKen();
+  //     res.cookie("access_token", token, {
+  //       httpOnly: true,
+  //       maxAge: 1000 * 60 * 60 * 24 * 7
+  //     });
+  //     return res.status(200).json(user);
+  //   })
+  //   .catch(e => console.log(e));
 };
 
-const register = (req, res) => {
+const register = async (req, res) => {
   console.log(req.body);
   const { name, password } = req.body;
-  if (!password) return res.status(405).json({ error: "password empty" });
+  if (!name || !password)
+    return res.status(405).json({ error: "공백을 채워주세요" });
 
   const user = new User({
     name,
     password
   });
+  try {
+    const exists = await User.findByName(name);
+    if (exists) return res.status(409).json({ error: "이미 존재하는 아이디" });
+
+    await user.save();
+
+    const token = user.generateToKen();
+    res.cookie("access_token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    });
+    return res.status(200).json(user.showUser());
+  } catch (e) {
+    console.log(e);
+  }
+  /*
   User.findByName(name)
     .then(exists => {
       if (exists)
         return res.status(409).json({ error: "이미 존재하는 아이디" });
-      return user
-        .save()
-        .then(user => res.status(200).json(user))
-        .catch(e => console.log(e));
+      return user.save();
     })
+    .then(user => res.status(200).json(user))
     .catch(e => console.log(e));
   const token = user.generateToKen();
   res.cookie("access_token", token, {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 7
   });
+  */
 };
 
 const check = (req, res) => {
--- server/models/user.js
+++ server/models/user.js
@@ -20,27 +20,33 @@ UserSchema.pre("save", function(next) {
   next(err);
 });
 
+UserSchema.methods.showUser = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+}
+
 UserSchema.methods.comparePassword = function(password) {
   return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToKen = function() {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+      name: this.name
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "3d"
+    }
+  );
+  return token;
 };
 
 UserSchema.statics.findByName = function(name) {
   return this.findOne({ name });
 };
-
-UserSchema.methods.generateToKen = function() {
-    const token = jwt.sign(
-        {
-            _id: this.id,
-            name: this.name,
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: '3d'
-        },
-    );
-    return token;
-}
 
 const User = mongoose.model("User", UserSchema);
 
